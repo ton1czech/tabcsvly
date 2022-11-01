1a569061258138a985824a32c8681cf608768eee
ton1czech <tonyasek007@gmail.com>
2022-11-01 20:31:02 +0100

    🎁 NEW: get the title a loop through rows

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import csv  
 
-# url = input('Type your URL: ')
-url = 'https://en.wikipedia.org/wiki/BMW_3_Series_(E90)'
+url = input('Type your URL: ')
 file_name = input('Choose .csv file name: ')
 content = []
 
@@ -13,17 +12,14 @@ table = soup.find_all('table')[1]
 
 rows = table.find_all('tr')
 
+for title in rows[0].find_all('th'):
+    content.append(title.text)
+
 for row in rows:
     items = row.find_all('td')
-    titles = rows[0].find('th')
-    print(rows[0])
-    print(titles)
-    content.append(titles)
+    for item in items:
+        content.append(item.text)
 
-    with open (f'{file_name}.csv', 'w') as f:
-        writer = csv.writer(f)
-
-        writer.writerow(content[0])
-        writer.writerows(content[1:])
-
-print(items)
+with open (f'{file_name}.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(content)
