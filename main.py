267b9028ee0bb8b9659066574b5006f31f60727f
ton1czech <tonyasek007@gmail.com>
2023-03-07 23:44:32 +0100

    👌 IMPROVE: using pd to get html tables

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import csv  
+import pandas as pd
 
 url = input('Type your URL: ')
+num = int(input('What table (number): '))
 file_name = input('Choose .csv file name: ')
-content = []
 
 soup = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'html.parser')
 
-table = soup.find_all('table')[1]
+table = str(soup.find_all('table')[num])
 
-rows = table.find_all('tr')
+df = pd.read_html("".join(table))[0]
 
-for title in rows[0].find_all('th'):
-    content.append(title.text)
-
-for row in rows:
-    items = row.find_all('td')
-    for item in items:
-        content.append(item.text)
-
-with open (f'{file_name}.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(content)
+df.to_csv(f'{file_name}.csv', index=False)
