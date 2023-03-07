6a394fbd15e18a116d946c680fab860221dc5d66
ton1czech <tonyasek007@gmail.com>
2023-03-07 23:49:13 +0100

    👌 IMPROVE: help

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pandas as pd
 
-url = input('Type your URL: ')
-num = int(input('What table (number): '))
-file_name = input('Choose .csv file name: ')
+url = input('type your URL [http://...]: ')
+num = int(input('what table [1-99] (from above): '))
+file_name = input('choose file name [a-z]: ')
 
 soup = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'html.parser')
 
-table = str(soup.find_all('table')[num])
+table = str(soup.find_all('table')[num-1])
 
 df = pd.read_html("".join(table))[0]
 
