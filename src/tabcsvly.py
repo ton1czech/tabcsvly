3e0565e2453d3bbe4ddca2cde431b70d132c9416
ton1czech <tonyasek007@gmail.com>
2023-03-10 00:23:03 +0100

    🎁 NEW: add comments

--- src/tabcsvly.py
+++ src/tabcsvly.py
@@ -4,22 +4,32 @@
 # Daniel Anthony Baudyš (ton1czech) #
 #####################################
 
+# import modules
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pandas as pd
 
+# get user url
 url = input('type your URL [http://...]: ')
+# get user table
 num = int(input('what table [1-99] (from above): '))
-file_name = input('choose file name [a-z]: ')
+# get user filename
+filename = input('choose file name [a-z]: ')
 
 def main():
+    # init scraper
     soup = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'html.parser')
 
+    # fint html table
     table = str(soup.find_all('table')[num-1])
 
+    # use pandas to red html table
     df = pd.read_html("".join(table))[0]
 
-    df.to_csv(f'{file_name}.csv', index=False)
+    # show the result
+    print(df)
+    # save data into .csv file
+    df.to_csv(f'{filename}.csv', index=False)
 
 if __name__ == "__main__":
     main()
